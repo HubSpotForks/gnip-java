d878963c3d2b04f69041967da68ca8fa2c05d3ed
Edward O'Neil <ekoneil@eko-mac.boulder.gnipcentral.com>
2008-11-13 15:38:56 -0700

    Updates to the Java client library:
    - put the .delete calls in finally {...} blocks in the tests
    - add an Error object to handle parsing JAXB error messages
    - add error messages from the server to exceptions that are thrown when bad things happen with a GnipConnection
    - add Publisher name to the Activities object

---
+++ src/main/java/com/gnipcentral/client/resource/Error.java
@@ -0,0 +1,11 @@
+package com.gnipcentral.client.resource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ekoneil
+ * Date: Nov 11, 2008
+ * Time: 8:32:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Error {
+}
